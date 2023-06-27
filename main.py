a2cece699af06229b5ef84233f89e9b58cf6fe4f
Marc Doerflinger <marc@doerf.li>
2023-06-27 15:19:13 +0000

    add parameter verification

--- main.py
+++ main.py
@@ -30,6 +30,18 @@ def hello_world() -> Message:
 def create_policy(request: PolicyRequest, response: Response, token: str = Depends(token_auth_scheme)) -> Message:
     """Create policy endpoint"""
     print(request)
+
+    if request.name.strip() == "":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Name cannot be empty")
+    if request.phone.strip() == "":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Phone cannot be empty")
+    if request.premium <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Premium must be greater than 0")
+    if request.suminsured <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Sum insured must be greater than 0")
+    if request.premium > request.suminsured:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Premium cannot be greater than sum insured")
+
     return {
         "message": "Policy created for %s!" % request.name
     }
